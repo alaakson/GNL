fcb1c02abac709c3df6920ac4b82985cc7a2a19a
alaakson <162610032+alaakson@users.noreply.github.com>
2024-07-09 15:02:54 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,88 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+//
 //First approach get_next_line
-#include "get_next_line.h"
-
-static void		*freedom(char **ptr);
-char			*ft_calloc(size_t count, size_t size);
-
-char	*get_next_line(int fd)
+//
+static void	*ft_calloc(size_t count, size_t size)
 {
-	static char	*content;
-    char		*line;
-    char		*buffer;
-	
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0 , 0) < 0)
-	{
-		freedom (&content);
-		return (NULL);
-	}
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	/*if (!buffer)
-	{
-		free (buffer);
-		return (NULL);
-	}*/
-	line = length_of_fd(fd, buffer, content);
-	free (buffer);
-	if (!line || line[0] == '\0')
-	{
-		free (content);
-		return (NULL);
-	}
-	content = line_extract(line);
-	if (content && content[0] == '\0')
-		freedom (&content);
-	return (line);
-}
+	void	*ptr;
 
-static char	*line_extract(char *line)
-{
-    size_t	length;
-    char	*storage;
-	
-	length = 0;
-	if (!line)
-		return (NULL);
-	while (line[length] != '\n' && line[length] != '\0')
-		length++;
-    if(line[length] == 0 || line[1] == 0)
-		return (NULL);
-	storage = ft_strdup(line + length + 1);
-	if (!storage)
-		return (NULL);
-	line[length + 1] = '\0';
-	return (storage);
-}
-
-static char	*length_of_fd(int fd, char *buf, char *storage)
-{
-	int	read_lines;
-	char	*temp;
-
-	read_lines = 1;
-	if (!read_lines)
-		return (NULL);
-	while (read_lines > 0)
+	ptr = malloc(count * size);
+	if (ptr)
 	{
-		read_lines = read(fd, buf, BUFFER_SIZE);
-		if (read_lines == -1)
-			return (NULL);
-		else if (read_lines == 0)
-			break ;
-		buf[read_lines] = 0;
-		if (!storage)
-			storage = ft_strdup("");
-		temp = storage;
-		storage = ft_strjoin(temp, buf);
-		free (temp);
-		if (!storage)
-			return (NULL);
-		if (ft_strchr(storage, '\n'))
-			break ;
+		ft_memset(ptr, 0, count * size);
 	}
-	return (storage);
+	return (ptr);
 }
 
 static void	*freedom(char **ptr)
@@ -104,26 +36,82 @@ static void	*freedom(char **ptr)
 	return (NULL);
 }
 
-char	*ft_calloc(size_t count, size_t size)
+static char	*line_extract(char *line)
 {
-	char	*ptr;
-	size_t			i;
+	size_t	length;
+	char	*storage;
 
-	if (count > 0 && size > 0 && count > (SIZE_MAX / size))
+	length = 0;
+	if (!line)
 		return (NULL);
-	ptr = malloc(count * size);
-	if (!ptr)
+	while (line[length] != '\n' && line[length] != '\0')
+		length++;
+	if (line[length] == '\0')
 		return (NULL);
-	i = 0;
-	while (i < count * size)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return (ptr);
+	storage = ft_strdup(line + length + 1);
+	if (!storage)
+		return (NULL);
+	line[length + 1] = '\0';
+	return (storage);
 }
 
+static char	*length_of_fd(int fd, char *buf, char *storage)
+{
+	int		read_lines;
+	char	*temp;
+
+	read_lines = 1;
+	while (read_lines > 0)
+	{
+		read_lines = read(fd, buf, BUFFER_SIZE);
+		if (read_lines == -1)
+			return (NULL);
+		else if (read_lines == 0)
+			break ;
+		buf[read_lines] = '\0';
+		if (!storage)
+			storage = ft_strdup("");
+		temp = storage;
+		storage = ft_strjoin(temp, buf);
+		free(temp);
+		if (!storage)
+			return (NULL);
+		if (ft_strchr(storage, '\n'))
+			break ;
+	}
+	return (storage);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*content;
+	char		*line;
+	char		*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0 , 0) < 0)
+	{
+		freedom (&content);
+		return (NULL);
+	}
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if(!buffer)
+		return (NULL);
+	line = length_of_fd(fd, buffer, content);
+	free(buffer);
+	if (!line)
+	{
+		free (content);
+		return (NULL);
+	}
+	content = line_extract(line);
+	if (content && content[0] == '\0')
+		freedom(&content);
+	return (line);
+}
+
+//
 //Second approach for get_next_line
+//
 include "get_next_line.h"
 
 static void	*freedom(char **ptr)
